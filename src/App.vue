f553e758c7e09551dcef1f1bfb222eb03dac1967
Stefan Jeremic <stefan.j@whitecitysoft.com>
2018-10-04 16:51:54 +0200

    fixed long animation

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ body {
 }
 
 .fade-enter-active, .fade-leave-active {
-  transition: opacity 1.8s;
+  transition: opacity .5s;
 }
 
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
